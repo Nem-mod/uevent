524540648487dad8e00bf44a5f2c990cbf30726c
northwind08 <zseveryn.lipovetskyz@gmail.com>
2024-04-12 19:48:12 +0300

    FEAT: Box component, EventCard component

---
+++ web-app/src/components/core/EventCard/EventCard.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+interface CardTypes {
+    title: string,
+    start: Date,
+    price: string,
+    tag: string,
+    address: string,
+    img?: string
+}
+const EventCard = ({title, start, price, tag, address}: CardTypes) => {
+    return (
+        <div className={'flex flex-row border rounded-md border-primary'}>
+            <img src="" alt="This is an image"/>
+            <div className={'flex flex-col py-2 px-4 gap-4 bg-secondary'}>
+                <div className={'flex flex-col gap-2'}>
+                    <span className={'text-black text-m font-semibold'}>
+                        {start.toDateString()}
+                    </span>
+                    <span className={'text-gray-500 text-m'}>
+                        {tag}
+                    </span>
+                </div>
+                <div className={'flex flex-col my-2'}>
+                    <span className={'text-l text-black font-bold'}>
+                        {title}
+                    </span>
+                </div>
+                <div className={'flex flex-col gap-2'}>
+                    <span className={'text-gray-500 text-m'}>
+                        {address}
+                    </span>
+                    <span className={'text-gray-500 text-m'}>
+                        {price}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default EventCard;
---
+++ web-app/src/components/utils/Box/Box.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from "react";
+
+interface PropTypes {
+    children?: React.ReactNode,
+    className?: string
+}
+
+function Box({children, className}: PropTypes) {
+    return (
+        <div
+            className={`${className} border rounded-md border-primary bg-secondary p-4 gap-6 max-w-64`}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default Box;
