c8ddc0842a08c13b50f4d65317405455ed4acac3
nechamdod <nechamdod@gmail.com>
2024-04-23 16:33:12 +0300

    FEAT: add additional props in event card

--- web-app/src/components/core/EventCard/EventCard.tsx
+++ web-app/src/components/core/EventCard/EventCard.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
+import {Link} from "@nextui-org/react";
 
 interface CardTypes {
     title: string;
     start: Date;
-    price: string;
+    price: string | number;
     tag: string;
     address: string;
     img?: string;
+    href?: string
 }
-function EventCard({ title, start, price, tag, address, img }: CardTypes) {
+
+function EventCard({title, start, price, tag, address, img, href}: CardTypes) {
     return (
         <div className={'border-gray/70 w-xl flex flex-row rounded-xl border-2 shadow-xl'}>
-            <div className={'max-w-unit-40'}>
-                <img className={'rounded-l-lg'} src={img} alt="This is an image" />
-            </div>
-            <div className={'flex flex-col gap-4 bg-transparent px-4 py-2 pl-8 pr-20'}>
-                <div className={'my-2 flex flex-col'}>
-                    <span className={'text-l font-bold text-black text-xl'}>{title}</span>
+            <Link href={href}>
+                <div className={'max-w-unit-40'}>
+                    <img className={'rounded-l-lg'} src={img} alt="This is an image"/>
                 </div>
-                <div className={'flex flex-col gap-2'}>
+                <div className={'flex flex-col gap-4 bg-transparent px-4 py-2 pl-8 pr-20'}>
+                    <div className={'my-2 flex flex-col'}>
+                        <span className={'text-l font-bold text-black text-xl'}>{title}</span>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
                     <span className={'text-m font-semibold text-black'}>
                         {start.toDateString()}
                     </span>
-                    <span className={'text-m text-gray-500'}>{tag}</span>
+                        <span className={'text-m text-gray-500'}>{tag}</span>
+                    </div>
+                    <div className={'flex flex-col gap-2'}>
+                        <span className={'text-m text-gray-500'}>{address}</span>
+                        <span className={'text-m text-gray-500'}>{price}</span>
+                    </div>
                 </div>
-                <div className={'flex flex-col gap-2'}>
-                    <span className={'text-m text-gray-500'}>{address}</span>
-                    <span className={'text-m text-gray-500'}>{price}</span>
-                </div>
-            </div>
+            </Link>
         </div>
     );
 }
