a80789dc3eb661220c12591ed023dd776deacbf8
nechamdod <nechamdod@gmail.com>
2024-04-23 14:56:26 +0300

    FIX: move eventcard title to the top

--- web-app/src/components/core/EventCard/EventCard.tsx
+++ web-app/src/components/core/EventCard/EventCard.tsx
@@ -15,14 +15,14 @@ function EventCard({ title, start, price, tag, address, img }: CardTypes) {
                 <img className={'rounded-l-lg'} src={img} alt="This is an image" />
             </div>
             <div className={'flex flex-col gap-4 bg-transparent px-4 py-2 pl-8 pr-20'}>
+                <div className={'my-2 flex flex-col'}>
+                    <span className={'text-l font-bold text-black text-xl'}>{title}</span>
+                </div>
                 <div className={'flex flex-col gap-2'}>
                     <span className={'text-m font-semibold text-black'}>
                         {start.toDateString()}
                     </span>
                     <span className={'text-m text-gray-500'}>{tag}</span>
-                </div>
-                <div className={'my-2 flex flex-col'}>
-                    <span className={'text-l font-bold text-black'}>{title}</span>
                 </div>
                 <div className={'flex flex-col gap-2'}>
                     <span className={'text-m text-gray-500'}>{address}</span>
