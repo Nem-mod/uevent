d68bb70593b828c67a81d938e8e222ea5afceeb9
nechamdod <nechamdod@gmail.com>
2024-04-23 12:42:24 +0300

    FIX: links in org navbar

--- web-app/src/components/organization/NavBarItem/NavBarItem.tsx
+++ web-app/src/components/organization/NavBarItem/NavBarItem.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {Link} from "@nextui-org/react";
 
 interface Props {
-    title: string
+    title: string,
+    href: string
 }
-function NavBarItem({title}: Props) {
+function NavBarItem({title, href}: Props) {
     return (
-            <Link href={'org/events'} aria-current={'page'} className={'w-full text-white text-xl'}>{title}</Link>
+            <Link href={href} aria-current={'page'} className={'w-full text-white text-xl'}>{title}</Link>
     );
 }
 
