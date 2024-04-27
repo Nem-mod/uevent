70bdf3efcd5afa88d950f1b8947c8c57968b99ad
nechamdod <nechamdod@gmail.com>
2024-04-27 23:22:33 +0300

    FIX: quick fix

--- web-app/src/app/(userside)/profile/page.tsx
+++ web-app/src/app/(userside)/profile/page.tsx
@@ -1,23 +1,24 @@
-'use client'
+'use client';
 
-import React, {useEffect, useState} from 'react';
-import Box from "@/components/utils/Box/Box";
-import {Button, Link} from "@nextui-org/react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure} from "@nextui-org/react";
-import {useRouter} from "next/navigation";
-import {authService} from "@/services/auth.service";
+import React, { useEffect, useState } from 'react';
+import Box from '@/components/utils/Box/Box';
+import { Button, Link } from '@nextui-org/react';
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue } from '@nextui-org/react';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
+import { authService } from '@/services/auth.service';
+import { IUserRegisterAndAuthRes } from '@/types/user.types';
 
 function Page() {
 
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-    const router = useRouter()
-    const [user, setUser] = useState();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const router = useRouter();
+    const [user, setUser] = useState<IUserRegisterAndAuthRes>();
 
     useEffect(() => {
         authService.getMe().then((res) => {
             setUser(res);
-        })
+        });
     }, []);
 
     // TODO: add logic
@@ -26,12 +27,12 @@ function Page() {
 
         console.log('Account deleted');
         router.replace('/');
-    }
+    };
 
     const columns = [
         {
             key: 'title',
-            label: 'TITLE'
+            label: 'TITLE',
         },
         {
             key: 'startDate',
@@ -39,130 +40,133 @@ function Page() {
         },
         {
             key: 'location',
-            label: 'LOCATION'
-        }
-    ]
+            label: 'LOCATION',
+        },
+    ];
 
     const tickets = [
         {
             id: '1',
             title: 'All bitches here',
             startDate: new Date().toDateString(),
-            location: 'London is the capital of Great Britain'
+            location: 'London is the capital of Great Britain',
         },
         {
             id: '2',
             title: 'No bitches D:',
             startDate: new Date().toDateString(),
-            location: 'London is the capital of Great Britain'
+            location: 'London is the capital of Great Britain',
         },
         {
             id: '3',
             title: 'Still no bitches',
             startDate: new Date().toDateString(),
-            location: 'London is the capital of Great Britain'
+            location: 'London is the capital of Great Britain',
         },
         {
             id: '4',
             title: 'Nigga balls',
             startDate: new Date().toDateString(),
-            location: 'London is the capital of Great Britain'
-        }
-    ]
+            location: 'London is the capital of Great Britain',
+        },
+    ];
+
 
     return (
         <div className={'p-10 grid place-items-center'}>
             <span className={'text-6xl font-extrabold text-black m-auto pt-4 mb-6'}>
                 Profile
             </span>
-            <Box className={'flex w-4/6 flex-col gap-4 p-8 py-14'}>
+            {user && (
+
+                <Box className={'flex w-4/6 flex-col gap-4 p-8 py-14'}>
 
                 <span className={'text-3xl font-extrabold text-black'}>
                     {user.username}
                 </span>
-                <span className={'text-2xl font-bold text-gray-700'}>
+                    <span className={'text-2xl font-bold text-gray-700'}>
                     {user.email}
                 </span>
-                <div className={'flex flex-col ml-auto gap-2'}>
-                    <Button
-                        className={
-                            'mt-auto ml-auto h-12 border border-primary bg-accent text-white hover:bg-accent ' +
-                            'w-fit text-lg font-semibold hover:border-accent hover:text-white'
-                        }
-                    >
-                        Reset password
-                    </Button>
-                    <Button
-                        href={'/profile/edit'}
-                        as={Link}
-                        className={
-                            'mt-auto ml-auto h-12 border border-primary bg-accent text-white hover:bg-accent ' +
-                            'w-fit text-lg font-semibold hover:border-accent hover:text-white'
-                        }
-                    >
-                        Edit profile
-                    </Button>
-                    <Button
-                        onPress={onOpen}
-                        color={'danger'}
-                        className={
-                            'mt-auto ml-auto h-12 text-white ' +
-                            'w-fit text-lg font-semibold hover:border-accent hover:text-white'
-                        }
-                    >
-                        Delete Account
-                    </Button>
-                    <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-                        <ModalContent>
-                            {(onClose) => (
-                                <>
-                                    <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                                    <ModalBody>
+                    <div className={'flex flex-col ml-auto gap-2'}>
+                        <Button
+                            className={
+                                'mt-auto ml-auto h-12 border border-primary bg-accent text-white hover:bg-accent ' +
+                                'w-fit text-lg font-semibold hover:border-accent hover:text-white'
+                            }
+                        >
+                            Reset password
+                        </Button>
+                        <Button
+                            href={'/profile/edit'}
+                            as={Link}
+                            className={
+                                'mt-auto ml-auto h-12 border border-primary bg-accent text-white hover:bg-accent ' +
+                                'w-fit text-lg font-semibold hover:border-accent hover:text-white'
+                            }
+                        >
+                            Edit profile
+                        </Button>
+                        <Button
+                            onPress={onOpen}
+                            color={'danger'}
+                            className={
+                                'mt-auto ml-auto h-12 text-white ' +
+                                'w-fit text-lg font-semibold hover:border-accent hover:text-white'
+                            }
+                        >
+                            Delete Account
+                        </Button>
+                        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+                            <ModalContent>
+                                {(onClose) => (
+                                    <>
+                                        <ModalHeader className='flex flex-col gap-1'>Modal Title</ModalHeader>
+                                        <ModalBody>
                                         <span className={'text-black font-semibold text-2xl'}>
                                             Are you sure you want to delete your account?
                                         </span>
-                                        <span className={'text-black font-semibold text-2xl'}>
+                                            <span className={'text-black font-semibold text-2xl'}>
                                             It will be lost forever
                                         </span>
-                                    </ModalBody>
-                                    <ModalFooter>
-                                        <Button color="primary" variant="light" onPress={onClose}>
-                                            Cancel
-                                        </Button>
-                                        <form onSubmit={handleDelete}>
-                                            <Button type={'submit'} color="danger" onPress={onClose}>
-                                                Delete
+                                        </ModalBody>
+                                        <ModalFooter>
+                                            <Button color='primary' variant='light' onPress={onClose}>
+                                                Cancel
                                             </Button>
-                                        </form>
-                                    </ModalFooter>
-                                </>
-                            )}
-                        </ModalContent>
-                    </Modal>
-                </div>
-                <span className={'text-5xl font-extrabold text-black'}>
+                                            <form onSubmit={handleDelete}>
+                                                <Button type={'submit'} color='danger' onPress={onClose}>
+                                                    Delete
+                                                </Button>
+                                            </form>
+                                        </ModalFooter>
+                                    </>
+                                )}
+                            </ModalContent>
+                        </Modal>
+                    </div>
+                    <span className={'text-5xl font-extrabold text-black'}>
                 Purchase history:
             </span>
-                <Table aria-label="Purchase history">
-                    <TableHeader columns={columns}>
-                        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-                    </TableHeader>
-                    <TableBody items={tickets}>
-                        {(item) => (
-                            <TableRow
-                                key={item.id}
-                            >
-                                {(columnKey) => <TableCell
-                                    className={'text-black'}
+                    <Table aria-label='Purchase history'>
+                        <TableHeader columns={columns}>
+                            {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+                        </TableHeader>
+                        <TableBody items={tickets}>
+                            {(item) => (
+                                <TableRow
+                                    key={item.id}
                                 >
-                                    {getKeyValue(item, columnKey)}
-                                </TableCell>}
-                            </TableRow>
-                        )}
-                    </TableBody>
-                </Table>
-            </Box>
-
+                                    {(columnKey) => <TableCell
+                                        className={'text-black'}
+                                    >
+                                        {getKeyValue(item, columnKey)}
+                                    </TableCell>}
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </Box>
+            )}
         </div>
     );
 }
