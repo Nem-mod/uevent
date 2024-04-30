78402c47cd3afb7edb62892123106d9bcf0efeba
nechamdod <nechamdod@gmail.com>
2024-04-30 19:05:32 +0300

    FEAT: logout

--- web-app/src/services/auth.service.ts
+++ web-app/src/services/auth.service.ts
@@ -1,6 +1,6 @@
-import {IUserAuthForm, IUserRegisterAndAuthRes, IUserRegisterForm, User} from "@/types/user.types";
+import { IUserAuthForm, IUserRegisterAndAuthRes, IUserRegisterForm, User } from '@/types/user.types';
 import { axiosWithAuth, axiosWithoutAuth } from '@/api/interseptors';
-import {AxiosError} from "axios";
+import { AxiosError } from 'axios';
 
 export const authService = {
     async login(data: IUserAuthForm): Promise<IUserRegisterAndAuthRes | undefined> {
@@ -9,7 +9,7 @@ export const authService = {
             return response.data;
         } catch (error) {
             if (error instanceof AxiosError && error.response)
-                throw new AxiosError(`${error.response.data.message}`)
+                throw new AxiosError(`${error.response.data.message}`);
         }
     },
     async register(data: IUserRegisterForm): Promise<IUserRegisterAndAuthRes> {
@@ -17,7 +17,7 @@ export const authService = {
             const response = await axiosWithoutAuth.post('/users/register', data);
             return response.data;
         } catch (e) {
-            throw new AxiosError('Registration failed, user already exists')
+            throw new AxiosError('Registration failed, user already exists');
         }
     },
 
@@ -26,17 +26,28 @@ export const authService = {
             const response = await axiosWithAuth.get('/users/me');
             return response.data;
         } catch (e) {
-            throw new AxiosError('Auth Failed')
+            throw new AxiosError('Auth Failed');
         }
     },
 
     async sendVerificationLink(returnLink: string, userId: number | string) {
-        const response = await axiosWithoutAuth.post(`/auth/user/${userId}/send/verify`, {returnLink});
-        return true
+        const response = await axiosWithoutAuth.post(`/auth/user/${userId}/send/verify`, { returnLink });
+        return true;
     },
 
     async confirmVerification(token: string, userId: string) {
-        const response = await axiosWithoutAuth.patch(`/auth/user/${userId}/validate/verify`, {token})
-        return true
-    }
-}
+        const response = await axiosWithoutAuth.patch(`/auth/user/${userId}/validate/verify`, { token });
+        return true;
+    },
+
+
+    async logout() {
+        try {
+            const response = await axiosWithoutAuth.delete(`/auth/user/logout`);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    },
+
+};
